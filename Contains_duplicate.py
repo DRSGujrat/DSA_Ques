a1919b8d13dc982d12b44f46b17ef802d36013a7
DRSGujrat <shahdev2022007@gmail.com>
2026-01-21 21:32:35 +0530

    Arrays and hashing Question 1

---
+++ Contains_duplicate.py
@@ -0,0 +1,26 @@
+#/ Contains Duplicate
+
+#// Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+#// Example 1:
+
+#// Input: nums = [1, 2, 3, 3]
+
+#// Output: true
+
+#// Example 2:
+
+#// Input: nums = [1, 2, 3, 4]
+
+#// Output: false
+
+l1 = []
+
+S1 = {i for i in l1}
+
+if len(S1) == len(l1):
+    print("False")
+else:
+    print("True")
+
+
