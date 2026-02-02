439338f8dc3b5e5f4b255af10f568e4cd8178dc0
DRSGujrat <shahdev2022007@gmail.com>
2026-02-02 20:11:02 +0530

    Add dailyTemperatures implementation in Java and Python

---
+++ java/stacks/dailyTemp.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] res = new int[temperatures.length];
+        Stack<Integer> stack = new Stack<>(); 
+
+        for (int i = 0; i < temperatures.length; i++) {
+            int t = temperatures[i];
+            while (!stack.isEmpty() && t > temperatures[stack.peek()]) {
+                int index = stack.pop();
+                res[index] = i - index;
+            }
+            stack.push(i);
+        }
+        return res;
+    }
+}
